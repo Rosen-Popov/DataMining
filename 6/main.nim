cd04e01683f9b0137aaa1964a5eee0a28900411e
Rosen Popov <rdonkow@gmail.com>
2020-12-27 21:34:14 +0200

    tmp

--- 6/main.nim
+++ 6/main.nim
@@ -5,41 +5,50 @@ import math
 const file_name:string = "./breast-cancer.data"
 
 type
-  node = object
+  node = ref object
     col:int
     value:int
     is_terminal:bool
-    children:seq[ref node]
-  section = object
+    children:seq[node]
+  
+  section = tuple[col:int,value:int]
+
+proc inside(vector:seq[int],criteria:seq[section]):bool=
+  for i in items(criteria):
+    if i.value == -1:
+      continue
+    if vector[i.col] != i.value:
+      return false
+  return true
 
 proc Rad(x:float64):float64=
   return x * x.log2()
 
-proc CalcEntropy(fr:frame,col:int,class:seq[int]= @[]):float{.discardable.}=
+proc CalcEntropy(fr:frame,col:int,class:seq[section]= @[]):float{.discardable.}=
   var counts:seq[int]
-  var length:int = fr.entries.len()
+  var length:int
   var res:float64 = 0
-  counts.setLen(fr.enumeration[0].len())
+  counts.setLen(fr.enumeration[col].len())
   for i in items(fr.entries):
-    if class.len() == 0:
+    if inside(i,class):
       inc(counts[i[col]])
-    elif i[col] in class:
-      inc(counts[i[col]])
+  length = sum(counts)
   for i in 0..counts.high():
-    if class.len() == 0:
-      res = res + Rad(counts[i]/length)
-    elif i in class:
+    if counts[i]!=0:
       res = res + Rad(counts[i]/length)
   return -res
 
-
-proc MakeDescTree(fr:frame,target_col:int,taken_cols:seq[int]):ref node=
+proc MakeDescTree(fr:frame,target_class:section,class:seq[section]):node
+  var children:seq[node]
+  block usual_case:
+    #calc entropy
+    #get biggest
+    return children
+    #split for that
+    #make nodes
+    break usual_case
   return nil
 
 if isMainModule:
   var res = AutoEnum(ReadData(file_name))
-
-
-
-
-
+  #echo CalcEntropy(res,0,@[(0,1)])
